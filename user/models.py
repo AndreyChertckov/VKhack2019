6ac16b207ba2b91e825bcda6f66b810a753d9d33
Anna Lvova <monstrolog@gmail.com>
2019-09-27 21:51:38 +0300

    edit user model

--- user/models.py
+++ user/models.py
@@ -5,6 +5,14 @@ from django.utils import timezone
 class User(models.Model):
     clock = models.TimeField()
 
+    @property
+    def daily_minus(self):
+        return filter(lambda x: x.action.time_effect < 0, self.logs)
+
+    @property
+    def daily_plus(self):
+        return filter(lambda x: x.action.time_effect > 0, self.logs)
+
     def create(self):
         self.save()
 
@@ -15,9 +23,9 @@ class Action(models.Model):
 
 
 class Log(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='user')
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='logs')
     time = models.DateTimeField()
-    action = models.ForeignKey(Action, null=True, on_delete=models.CASCADE, related_name='action')
+    action = models.ForeignKey(Action, null=True, on_delete=models.CASCADE, related_name='logs')
 
     def create(self):
         self.time = timezone.now()
