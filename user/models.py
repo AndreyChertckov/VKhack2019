736f1fd571f5e52c204fb24ce36f383b1310131a
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-09-27 22:51:03 +0300

    brfore, after in log

--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 class User(models.Model):
@@ -27,9 +28,29 @@ class Log(models.Model):
     time = models.DateTimeField()
     action = models.ForeignKey(Action, null=True, on_delete=models.CASCADE, related_name='logs')
 
+    @property
+    def before(self):
+        current = self.user.clock
+        for log in self.user.logs:
+            if log.time > self.time:
+                if log.action.time_effect > 0:
+                    current = (current - datetime.timedelta(minutes=log.action.time_effect)).time()
+                else:
+                    current = (current + datetime.timedelta(minutes=((-1) * log.action.time_effect))).time()
+        return current
+
+    @property
+    def after(self):
+        if self.action.time_effect > 0:
+            return (self.before + datetime.timedelta(minutes=self.action.time_effect)).time()
+        else:
+            return (self.before - datetime.timedelta(minutes=((-1) * self.action.time_effect))).time()
+
     def create(self):
         self.time = timezone.now()
         self.save()
 
 
-
+class Fact(models.Model):
+    description = models.TextField()
+    appearance_time = models.TimeField()
