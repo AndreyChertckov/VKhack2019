d99079702627ca1405c0687fe8aa40cf4c8ba496
Anna Lvova <monstrolog@gmail.com>
2019-09-28 11:03:04 +0300

    add user_weekly_logs view

--- user/urls.py
+++ user/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('api/user', views.create_user),
     path('api/user/clock', views.user_clock),
     path('api/user/logs', views.user_logs),
-    path('api/action', views.action_list)
+    path('api/action', views.action_list),
+    path('api/user/logs/week', views.user_weekly_logs)
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.db.models import Sum, Q
+from django.db.models.functions import TruncDate
+from django.db.models.fields import DateField
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, parser_classes
 from user.models import User, Clock, Log, Action
@@ -69,6 +72,14 @@ def user_weekly_logs(request):
         user = User.objects.get(pk=request.GET.get('token'))
     except:
         return HttpResponse(status=404)
+
+    last_7_days = datetime.datetime.today() - datetime.timedelta(7)
+    logs = Log.objects.filter(user=user.pk, time__gte=last_7_days)\
+        .annotate(day=TruncDate('time')).values('day').annotate(
+            plus_overall=Sum('action__time_effect', filter=Q(action__time_effect__gt=0)),
+            minus_overall=Sum('action__time_effect', filter=Q(action__time_effect__lt=0)))
+    
+    return JsonResponse(list(logs), safe=False)
 
 @api_view(['GET'])
 def user_monthly_logs(request):
